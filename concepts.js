54cd4234b06f00f41e2b7af83c0a58ed329436ca
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-01-21 15:59:06 +0100

    Added a few basic concepts with a brief description

---
+++ concepts.js
@@ -0,0 +1,19 @@
+// ## 1 ## Console
+console.log('Hello World!')
+
+// ## 2 ## Defining variables
+// 'var' was the common way to define variables but is now obsolete
+// 'let' is the common standard for determining a variable 
+// 'const' can be also used to determine a variable, but it's value cannot change after defining
+// a variable declared with the 'const' keyword must be assigned a value at the time of declaration.
+var varVariable
+let letVariable
+const constVariable = "value"
+
+// ## 3 ## Primitive data types
+let string = "Hello World"
+let number = 42
+let bigint = 42n
+let boolean = true
+let undefined
+let symbol = Symbol("my private property");
