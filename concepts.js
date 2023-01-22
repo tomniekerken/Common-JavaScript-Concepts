943b78695ef5a2d67572008e5a6186397d8ae9d5
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2023-01-22 18:14:03 +0100

    Detailed explanation to the higher-order and closure, changed comment structure

--- concepts.js
+++ concepts.js
@@ -1,7 +1,15 @@
-// ## 1 ## Console
+// #
+// #
+// 1 Console
+// #
+// #
 console.log('Hello World!')
 
-// ## 2 ## Defining variables
+// #
+// #
+// 2 Defining variables
+// #
+// #
 // 'var' was the common way to define variables but is now obsolete
 // 'var' will not apply to the block scope of ## 4 ##
 // 'let' is the common standard for determining a variable 
@@ -11,7 +19,11 @@ var varVariable
 let letVariable
 const constVariable = "value"
 
-// ## 3 ## Primitive data types
+// #
+// #
+// 3 Primitive data types
+// #
+// #
 // A primitive data type is a classification of data that dictates the type of value it can represent and the operations that can be performed on it.
 let string = "Hello World"
 let number = 42
@@ -21,7 +33,11 @@ let undefined
 let symbol = Symbol("my private property")
 let nullVar = null
 
-// ## 4 ## (Basic) Mathematical operations
+// #
+// #
+// 4  (Basic) Mathematical operations
+// #
+// #
 // These methematical operations do not contain operations done using math object methods
 let x = 1
 let y = 2
@@ -33,7 +49,11 @@ let division = x / y
 let modulus = x % y
 let exponentiation = x ** y
 
-// ## 5 ## Lexical environment
+// #
+// #
+// 5  Lexical environment
+// #
+// #
 // Determines where variables work and where they don't
 // Global scope | Variables can be accessed in every scope
 // Local scope | Variables become local for a specific scope, which could be a function
@@ -74,14 +94,18 @@ const functionExpression = function(x, y) {
 
 // This is a higher-order function because it takes a function as an argument
 // and it returns a function as a result
-// the returned function is also a closure since it has access to the multiplier variable
-// higher-order makes the code more reusable
+// The returned function is also a closure since it has access to the multiplier variable
+// Higher-order makes the code more reusable
+// Closures are often used to create private variables/methods, which can only be accessed and modified by the inner function
+// keeping them hidden and protected from the global scope
 function higherOrder(multiplier) {
-    return function(number) {
+    return function(number) { // This is the closure function because it has access to the 'multiplier' variable from its parent scope
         return number * multiplier
     }
 }
+
 let double = higherOrder(2)
 console.log(double(5)) // Output: 10
+
 let triple = higherOrder(3)
 console.log(triple(5)) // Output: 15
